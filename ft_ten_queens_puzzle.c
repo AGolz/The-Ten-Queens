567589b1e5752d5d98a98b9b0971c5c93a52bc63
Alyona Golz <golz.alyona@gmail.com>
2023-11-22 19:17:36 +0400

    Update ft_ten_queens_puzzle.c

--- ft_ten_queens_puzzle.c
+++ ft_ten_queens_puzzle.c
@@ -10,13 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <unistd.h>
 
-int ft_cons(int x, int board[])
+int	ft_cons(int x, int board[])
 {
-	int i;
-	int tmp;
-	
+	int	i;
+	int	tmp;
+
 	i = 0;
 	while (i < x)
 	{
@@ -30,10 +31,10 @@ int ft_cons(int x, int board[])
 	return (1);
 }
 
-void board_print(int board[])
+void	board_print(int board[])
 {
-	int i;
-	char tmp;
+	int		i;
+	char	tmp;
 
 	i = 0;
 	while (i < 10)
@@ -45,16 +46,16 @@ void board_print(int board[])
 	write(1, "\n", 1);
 }
 
-void is_queen(int x, int *cn, int board[])
+void	is_queen(int x, int *cn, int board[])
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (x == 10)
 	{
 		*cn += 1;
 		board_print(board);
-		return ; 
+		return ;
 	}
 	while (i < 10)
 	{
@@ -65,16 +66,16 @@ void is_queen(int x, int *cn, int board[])
 	}
 }
 
-int ft_ten_queens_puzzle(void)
+int	ft_ten_queens_puzzle(void)
 {
-	int cn;
-	int i;
-	int board[10];
-	
+	int	cn;
+	int	i;
+	int	board[10];
+
 	cn = 0;
 	i = 0;
 	while (i < 10)
 		board[i++] = 0;
 	is_queen(0, &cn, board);
-	return(cn);
+	return (cn);
 }
